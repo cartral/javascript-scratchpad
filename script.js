c31891a392d4bc8381e515d3a7cad1a913737162
Glitch (cartral-magic-8ball) <none>
2019-10-28 03:26:37 +0000

    ☔️🐧 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,46 @@
+function analyzeQuestion(question) {
+    return "Add your code here"
+}
+
+function clickAsk(question) {
+  var results = analyzeQuestion(question)
+  setAnswer(results)
+}
+
+
+function setAnswer(results) {
+    document.getElementById("gwc_answer").innerHTML = results
+}
+
+function getRandomNumber(low,high) {
+  return Math.round((Math.random() * (high - low)) + low);
+}
+
+function analyzeQuestionPrebuilt(question) {
+    if (question.length == 0) {
+      return "Please ask a question!"
+    }
+    var answers = ["It is certain", 
+                   "It is decidedly so", 
+                   "Without a doubt", 
+                   "Yes - definitely",
+                   "You may rely on it", 
+                   "As I see it, yes", 
+                   "Most likely", 
+                   "Outlook good", 
+                   "Yes", "Signs point to yes",
+                   "Don't count on it", 
+                   "My reply is no",
+                   "My sources say no", 
+                   "Outlook not so good",
+                   "Very doubtful", 
+                   "Reply hazy, try again", 
+                   "Ask again later", 
+                   "Better not tell you now",
+                   "Cannot predict now", 
+                   "Concentrate and ask again"];
+  
+  var random = getRandomNumber(0, answers.length);
+  return answers[random];
+}
+
